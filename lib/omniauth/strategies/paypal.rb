93f9cda2f706026f1426b2301502ca41355a623f
Oriol Francès <oriolfa@gmail.com>
2015-12-07 09:07:48 +0100

    Remove anything but openid stuff

--- lib/omniauth/strategies/paypal.rb
+++ lib/omniauth/strategies/paypal.rb
@@ -3,7 +3,7 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class PayPal < OmniAuth::Strategies::OAuth2
-      DEFAULT_SCOPE = "openid profile"
+      DEFAULT_SCOPE = "openid"
       DEFAULT_RESPONSE_TYPE = "code"
       SANDBOX_SITE = "https://api.sandbox.paypal.com"
       SANDBOX_AUTHORIZE_URL = 'https://www.sandbox.paypal.com/webapps/auth/protocol/openidconnect/v1/authorize'
@@ -19,34 +19,8 @@ module OmniAuth
       option :provider_ignores_state, true
       option :sandbox, false
 
-      uid { @parsed_uid ||= (/\/([^\/]+)\z/.match raw_info['user_id'])[1] } #https://www.paypal.com/webapps/auth/identity/user/baCNqjGvIxzlbvDCSsfhN3IrQDtQtsVr79AwAjMxekw => baCNqjGvIxzlbvDCSsfhN3IrQDtQtsVr79AwAjMxekw
-
-      info do
-        prune!({
-                   'name' => raw_info['name'],
-                   'email' => raw_info['email'],
-                   'first_name' => raw_info['given_name'],
-                   'last_name' => raw_info['family_name'],
-                   'given_name' => raw_info['given_name'],
-                   'family_name' => raw_info['family_name'],
-                   'location' => (raw_info['address'] || {})['locality'],
-                   'phone' => raw_info['phone_number']
-               })
-      end
-
-      extra do
-        prune!({
-                   'account_type' => raw_info['account_type'],
-                   'user_id' => raw_info['user_id'],
-                   'address' => raw_info['address'],
-                   'verified_account' => (raw_info['verified_account'] == 'true'),
-                   'language' => raw_info['language'],
-                   'zoneinfo' => raw_info['zoneinfo'],
-                   'locale' => raw_info['locale'],
-                   'account_creation_date' => raw_info['account_creation_date'],
-                   'age_range' => raw_info['age_range'],
-                   'birthday' => raw_info['birthday']
-               })
+      def callback_url
+        full_host + script_name + callback_path # + query_string
       end
 
       def setup_phase
@@ -57,32 +31,12 @@ module OmniAuth
         super
       end
 
-      def raw_info
-        @raw_info ||= load_identity()
-      end
-
       def authorize_params
         super.tap do |params|
           params[:scope] ||= DEFAULT_SCOPE
           params[:response_type] ||= DEFAULT_RESPONSE_TYPE
         end
       end
-
-      private
-        def load_identity
-          access_token.options[:mode] = :query
-          access_token.options[:param_name] = :access_token
-          access_token.options[:grant_type] = :authorization_code
-          access_token.get('/v1/identity/openidconnect/userinfo', { :params => { :schema => 'openid'}}).parsed || {}
-        end
-
-        def prune!(hash)
-          hash.delete_if do |_, value|
-            prune!(value) if value.is_a?(Hash)
-            value.nil? || (value.respond_to?(:empty?) && value.empty?)
-          end
-        end
-
     end
   end
 end
--- omniauth-paypal.gemspec
+++ omniauth-paypal.gemspec
@@ -9,15 +9,31 @@ Gem::Specification.new do |s|
   s.email    = ['david@datariot.io']
   s.summary  = 'PayPal Identity strategy for OmniAuth'
   s.homepage = 'https://github.com/datariot/omniauth-paypal'
-  s.license  = 'MIT'
+  s.licenses = ["MIT"]
 
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map { |f| File.basename(f) }
   s.require_paths = ['lib']
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.rubygems_version = "2.4.8"
+  s.date = "2015-12-03"
 
-  s.add_runtime_dependency 'omniauth-oauth2', '~> 1.4'
+  if s.respond_to? :specification_version then
+    s.specification_version = 4
 
-  s.add_development_dependency 'rspec', '~> 3.3.0'
-  s.add_development_dependency 'rake'
+    if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
+      s.add_runtime_dependency(%q<omniauth-oauth2>, ["~> 1.4"])
+      s.add_development_dependency(%q<rspec>, ["~> 3.3.0"])
+      s.add_development_dependency(%q<rake>, [">= 0"])
+    else
+      s.add_dependency(%q<omniauth-oauth2>, ["~> 1.4"])
+      s.add_dependency(%q<rspec>, ["~> 3.3.0"])
+      s.add_dependency(%q<rake>, [">= 0"])
+    end
+  else
+    s.add_dependency(%q<omniauth-oauth2>, ["~> 1.4"])
+    s.add_dependency(%q<rspec>, ["~> 3.3.0"])
+    s.add_dependency(%q<rake>, [">= 0"])
+  end
 end
